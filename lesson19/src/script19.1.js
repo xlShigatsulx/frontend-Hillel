afb564ba275b56ec03f5e6fecab0b29a8ee91b6c
Shigatsu <05rustik04@gmail.com>
2025-01-21 19:31:44 +0200

    edit hw 19.1

--- lesson19/src/script19.1.js
+++ lesson19/src/script19.1.js
@@ -1,11 +1,11 @@
-const form = document.forms.weatherForm;
+const form = document.forms[0];
 const selectElement = document.querySelector('select[name="choices"]');
-const weatherDetails = document.querySelector('.weatherDetails');
+const container = document.querySelector('.weatherDetails');
 
-class WeatherBox {
-    constructor(container = form){
-        this.container = container;
-    }
+class WeatherManager {
+	constructor(container) {
+		this.container = container;
+	}
 
 	createEl({ type = 'div', content, attributes } = {}) {
 		const $el = document.createElement(type)
@@ -24,77 +24,128 @@ class WeatherBox {
 		return $el
 	}
 
-    clearContent(container, flag) {
-        if (!container || container.nodeType !== 1) return;
+	clearContent(container, flag) {
+		if (!container || container.nodeType !== 1) return
 
-        while (container.firstElementChild) {
-            container.firstElementChild.remove();
-        }
+		while (container.firstElementChild) {
+			container.firstElementChild.remove()
+		}
 
-        if (flag) container.remove();
-    }
+		if (flag) container.remove()
+	}
 
-    removeIconAndButton() {
-        const icon = document.querySelector('.weatherIcon');
-        const updateButton = document.querySelector('.btnWeatherForm');
-        if (icon) icon.remove();
-        if (updateButton) updateButton.remove();
-    }
+	removeIconAndButton() {
+		const icon = document.querySelector('.weatherIcon')
+		const updateButton = document.querySelector('.btnWeatherUpDate')
+		if (icon) icon.remove()
+		if (updateButton) updateButton.remove()
+	}
 
+	getDateNow(locale = 'uk-UA') {
+		const now = new Date()
+		return now.toLocaleDateString(locale, {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+		})
+	}
 
-    getDateNow(locale = 'uk-UA') {
-        const now = new Date();
-        return now.toLocaleDateString(locale, {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-        });
-    }
+	getTimeNow(locale = 'uk-UA') {
+		const now = new Date()
+		return now.toLocaleTimeString(locale, {
+			hour: '2-digit',
+			minute: '2-digit',
+		})
+	}
 
-    getTimeNow(locale = 'uk-UA') {
-        const now = new Date();
-        return now.toLocaleTimeString(locale, {
-            hour: '2-digit',
-            minute: '2-digit',
-        });
-    }
+	createWeatherBox(data) {
+        const containerUl = this.createEl({
+            type: 'ul',
+            attributes: { class: "weatherInfo"}
+        })
+		const dateNow = this.createEl({
+			type: 'li',
+			content: `Дата: ${this.getDateNow()}`,
+		})
+		const timeNow = this.createEl({
+			type: 'li',
+			content: `Час: ${this.getTimeNow()}`,
+		})
+        const name = this.createEl({
+            type: 'li',
+            content: `Місце: ${data?.name}`
+        })
+		const humidity = this.createEl({
+			type: 'li',
+			content: `Вологість: ${data?.main?.humidity ?? 'N/A'} %`,
+		})
+		const pressure = this.createEl({
+			type: 'li',
+			content: `Тиск: ${data?.main?.pressure ?? 'N/A'} hPa`,
+		})
+		const temp = this.createEl({
+			type: 'li',
+			content: `Температура: ${
+				Math.round(data?.main?.temp - 273.15) ?? 'N/A'
+			} °C`,
+		})
+		const feelsLike = this.createEl({
+			type: 'li',
+			content: `Відчувається як: ${
+				Math.round(data?.main?.feels_like - 273.15) ?? 'N/A'
+			} °C`,
+		})
+		const wind = this.createEl({
+			type: 'li',
+			content: `Вітер: ${data?.wind?.speed ?? 'N/A'} м/с`,
+		})
+		const btnIcon = this.createEl({
+			type: 'i',
+			attributes: { class: 'fas fa-sync-alt' },
+		})
+		const btnUpDate = this.createEl({
+			type: 'button',
+			content: btnIcon,
+			attributes: { class: 'btnWeatherUpDate' },
+		})
+		const icon = this.createEl({
+			type: 'img',
+			attributes: {
+				src: `https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`,
+				class: 'weatherIcon',
+			},
+		})
 
-	createWeatherBox(data, container = this.container) {
-        const dateNow = this.createEl({ type: 'label', content: `Дата: ${this.getDateNow()}` });
-        const timeNow = this.createEl({ type: 'label', content: `Час: ${this.getTimeNow()}` });
-        const humidity = this.createEl({ type: 'label', content: `Вологість: ${data?.main?.humidity ?? 'N/A'} %` });
-        const pressure = this.createEl({ type: 'label', content: `Тиск: ${data?.main?.pressure ?? 'N/A'} hPa` });
-        const temp = this.createEl({ type: 'label', content: `Температура: ${Math.round(data?.main?.temp - 273.15) ?? 'N/A'} °C` });
-        const feelsLike = this.createEl({ type: 'label', content: `Відчувається як: ${Math.round(data?.main?.feels_like - 273.15) ?? 'N/A'} °C` });
-        const wind = this.createEl({ type: 'label', content: `Вітер: ${data?.wind?.speed ?? 'N/A'} м/с` });
-        const btnIcon = this.createEl({ type: 'i', attributes: {class: "fas fa-sync-alt"}});
-        const btnUpDate = this.createEl({ type: 'button', content: btnIcon, attributes: { class: "btnWeatherForm"}});
-        const icon = this.createEl({type: 'img', attributes: { src: `https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`, class: 'weatherIcon'}});
+		btnUpDate.addEventListener('click', async () => {
+			try {
+				this.removeIconAndButton()
+				this.clearContent(container)
 
-        btnUpDate.addEventListener('click', async () => {
-            try {
-                this.removeIconAndButton();
-                this.clearContent(container);
+				const cityName = selectElement.value
+				const weatherData = await fetchWeatherData(cityName)
 
-                const cityName = selectElement.value;
-                const weatherData = await fetchWeatherData(cityName);
-
-                this.createWeatherBox(weatherData, container);
-                console.log('Weather data updated successfully!');
-            } catch (error) {
-                console.error('Failed to update weather data:', error);
-            }
-        });
-
-        const fragment = document.createDocumentFragment();
-		fragment.append(dateNow, timeNow, humidity, pressure, temp, feelsLike, wind);
-        weatherDetails.append(icon, btnUpDate);
-        container.append(fragment);
+				this.createWeatherBox(weatherData)
+				console.log('Weather data updated successfully!')
+			} catch (error) {
+				console.error('Failed to update weather data:', error)
+			}
+		})
+		containerUl.append(
+			dateNow,
+			timeNow,
+            name,
+			humidity,
+			pressure,
+			temp,
+			feelsLike,
+			wind
+		)
+        container.append(containerUl, icon, btnUpDate)
 	}
 }
 
 async function fetchWeatherData(cityName) {
-    let weather;
+    let weatherData;
 
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},ua&appid=d0a8f0783e0adcf8a62cd353c611c113`);
     if (!res.ok) {
@@ -102,18 +153,65 @@ async function fetchWeatherData(cityName) {
     }
     const data = await res.json();
 
-    weather = data || [];
-    return weather;
+    weatherData = data || []
+    return weatherData
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
     const cityName = selectElement.value;
-    const weatherManager = new WeatherBox();
-    
+	const weatherManager = new WeatherManager(container);
     try {
-        const weatherData = await fetchWeatherData(cityName);
-        weatherManager.createWeatherBox(weatherData);
-    } catch (error) {
-        console.error('Failed to fetch initial weather data:', error);
-    }
+		weatherManager.removeIconAndButton()
+		weatherManager.clearContent(container)
+
+		const weatherData = await fetchWeatherData(cityName)
+
+		weatherManager.createWeatherBox(weatherData)
+		console.log('Weather data load successfully!')
+	} catch (error) {
+		console.error('Failed to load weather data:', error)
+	}
 });
+
+(async () => {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            async (position) => {
+                const { latitude, longitude } = position.coords;
+                const weatherManager = new WeatherManager(container);
+
+                    try {
+                        weatherManager.removeIconAndButton();
+                        weatherManager.clearContent(container);
+
+                        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d0a8f0783e0adcf8a62cd353c611c113`);
+                        const weatherData = await res.json();
+
+                        weatherManager.createWeatherBox(weatherData);
+                        console.log('Weather data load successfully!');
+                    } catch (error) {
+                        console.error('Failed to load weather data:', error);
+                    }
+            },
+            (error) => {
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        console.log("Користувач відхилив запит на геолокацію.");
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        console.log("Інформація про місцезнаходження недоступна.");
+                        break;
+                    case error.TIMEOUT:
+                        console.log("Перевищено час очікування.");
+                        break;
+                    default:
+                        console.log("Сталася невідома помилка.");
+                }
+            }
+        );
+    } else {
+        output.textContent = "Геолокація не підтримується вашим браузером.";
+    }
+})();
